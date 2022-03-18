08703edc867aa3b5cdcbfbda6df9b1f74d94f698
nwe3er <77739655+nwe3er@users.noreply.github.com>
2022-03-17 21:36:19 -0500

    Added the Search class

---
+++ Search.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+// Search class to find CSP
+public class Search {
+
+
+   // method to find CSP using backTracking
+   public String backTracking(ArrayList<String> listOfConstraints, ArrayList<Character> listOfVariables, ArrayList<ArrayList<Integer>> listOfVariableValues) {
+
+      return "CSP";
+   }
+   // method to find CSP using ForwardChecking
+   public String ForwardChecking(ArrayList<String> listOfConstraints, ArrayList<Character> listOfVariables, ArrayList<ArrayList<Integer>> listOfVariableValues) {
+
+
+      return "CSP";
+   }
+
+}
